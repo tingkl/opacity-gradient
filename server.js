4b2ad78e46eab1476c0727a9ed4847e60b417689
tingkl <dingguoliang01@baidu.com>
2014-09-17 13:26:57 +0800

    'resize'

--- server.js
+++ server.js
@@ -8,14 +8,58 @@ var path = require('path');
 var root = path.dirname(module.filename);
 var less = require('less');
 var querystring = require('querystring');
+var url = require('url');
 var parser = new (less.Parser)({
     paths: [path.join(__dirname, 'less')], // 指定@import搜索的目录
     filename: path.join(__dirname, 'less/' + 'error.less') // 为了更好地输出错误信息，可以指定一个文件名
 });
-function lessFilter(filePath, next) {
-    if (path.extname(filePath) === '.less') {
-        var cssPath = filePath.replace('.less', '.css');
-        var lessPath = filePath;
+var connect = require('connect');
+var app = connect();
+app.use(function (req, res, next) {
+    var opt = url.parse(req.url);
+    req.pathname = opt.pathname;
+    req.query = querystring.parse(opt.query);
+    next();
+});
+app.use(function (req, res, next) {
+    if (req.method === 'POST') {
+        var buffers = [];
+        var nread = 0;
+        req.on('data', function (chunk) {
+            buffers.push(chunk);
+            nread += chunk.length;
+        });
+        req.on('end', function () {
+            var buffer = null;
+            switch (buffers.length) {
+                case 0:
+                    buffer = new Buffer(0);
+                    break;
+                case 1:
+                    buffer = buffers[0];
+                    break;
+                default :
+                    buffer = new Buffer(nread);
+                    for (var i = 0, pos = 0, l = buffers.length; i < l; i++) {
+                        var chunk = buffers[i];
+                        chunk.copy(buffer, pos);
+                        pos += chunk.length;
+                    }
+                    break;
+            }
+            //保存POST数据
+            req.body = querystring.parse(buffer.toString());
+            next();
+        });
+    }
+    else {
+        next();
+    }
+});
+app.use(function (req, res, next) {
+    if (path.extname(req.pathname) === '.less') {
+        var lessPath = path.join(root, req.pathname);
+        var cssPath = lessPath.replace('.less', '.css');
         var out_of_date = false;
         if (!fs.existsSync(cssPath)) {
             out_of_date = true;
@@ -26,18 +70,19 @@ function lessFilter(filePath, next) {
                 out_of_date = true;
             }
         }
+        req.pathname = req.pathname.replace('.less', '.css');
         if (out_of_date) {
             fs.readFile(lessPath, 'utf-8', function (err, lessContent) {
                 parser.parse(lessContent, function (err, tree) {
                     if (err) {
-                        next(err, filePath);
+                        throw err;
                     } else {
                         fs.writeFile(cssPath, tree.toCSS({ compress: true }), 'utf-8', function (err) {
                             if (err) {
-                                next(err, filePath);
+                                throw err;
                             }
                             else {
-                                next(false, cssPath);
+                                next();
                             }
                         })
                     }
@@ -45,74 +90,62 @@ function lessFilter(filePath, next) {
             });
         }
         else {
-            next(false, cssPath);
+            next();
         }
     }
     else {
-        next(false, filePath);
+        next();
     }
+});
+function json(obj) {
+    this.writeHead(200, {'Content-Type': 'application/json'});
+    this.end(JSON.stringify(obj));
 }
-http.createServer(function (req, res) {
-    lessFilter(path.join(root, req.url), function (err, filePath) {
+function file(path) {
+    var me = this;
+    this.writeHead(200, {'Content-Type': mime.lookup(path)});
+    fs.readFile(path, function (err, data) {
         if (err) {
-            throw err;
+            console.log(err);
         }
         else {
-            if (filePath === path.join(root, 'update')) {
-                var _postData = '';
-                //提取POST数据
-                req.on('data', function (data) {
-                    _postData += data;
-                });
-                req.on('end', function () {
-                    //保存POST数据
-                    req.post = querystring.parse(_postData);
-                    var key = req.post.key;
-                    var func = req.post.func;
-                    fs.writeFileSync(path.join('lib', key), func);
-                    res.writeHead(200, {'Content-Type': 'text/plain'});
-                    res.end(JSON.stringify({key: key, func: func}));
-                });
-            }
-            else if (filePath === path.join(root, 'delete')) {
-                var _postData = '';
-                //提取POST数据
-                req.on('data', function (data) {
-                    _postData += data;
-                });
-                req.on('end', function () {
-                    //保存POST数据
-                    req.post = querystring.parse(_postData);
-                    var key = req.post.key;
-                    fs.unlinkSync(path.join('lib', key));
-                    res.writeHead(200, {'Content-Type': 'text/plain'});
-                    res.end(JSON.stringify({key: key}));
-                });
-            }
-            else if (filePath === path.join(root, 'map.js')) {
-                fs.readdir('lib', function(err, files) {
-                    var map = '{\n';
-                    files.forEach(function(item, index) {
-                        var data = fs.readFileSync(path.join(root, 'lib', item)).toString();
-                        map += '\'' + item + '\':' + data + ',\n';
-                    });
-                    map = map.substr(0, map.length - 2) + '\n}';
-                    res.writeHead(200, {'Content-Type': 'text/plain'});
-                    res.end(map);
-                });
-            }
-            else {
-                res.writeHead(200, {'Content-Type': mime.lookup(filePath)});
-                fs.readFile(filePath, function (err, data) {
-                    if (err) {
-                        console.log(err);
-                    }
-                    else {
-                        res.end(data);
-                    }
-                });
-            }
+            me.end(data);
         }
     });
-}).listen(3000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:3000/');
+}
+app.use(function(req, res, next) {
+    res.json = json;
+    res.file = file;
+    next();
+});
+app.use(function (req, res) {
+    if (req.pathname === '/update') {
+        var key = req.body.key;
+        var func = req.body.func;
+        fs.writeFileSync(path.join('lib', key), func);
+        res.json({key: key, func: func});
+    }
+    else if (req.pathname === '/delete') {
+        var key = req.body.key;
+        fs.unlinkSync(path.join('lib', key));
+        res.json({key: key});
+    }
+    else if (req.pathname === '/map.js') {
+        fs.readdir('lib', function (err, files) {
+            var map = '{\n';
+            files.forEach(function (item, index) {
+                var data = fs.readFileSync(path.join(root, 'lib', item)).toString();
+                map += '\'' + item + '\':' + data + ',\n';
+            });
+            map = map.substr(0, map.length - 2) + '\n}';
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(map);
+        });
+    }
+    else {
+        res.file(path.join(root, req.pathname));
+    }
+});
+var port = 9001;
+http.createServer(app).listen(port, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:' + port + '/');
